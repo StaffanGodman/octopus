08b9975de677eb14eddf6e9c2a77f32e3110e453
björn munthe <bjomun1011@skola.goteborg.se>
2021-03-17 13:47:16 +0100

    more bugfix adding https

--- src/components/areaMenu.vue
+++ src/components/areaMenu.vue
@@ -50,7 +50,7 @@ export default {
       if (event.target.value === "all") {
         try {
           let resp = await fetch(
-            "http://api.sr.se/api/v2/traffic/messages?format=json"
+            "https://api.sr.se/api/v2/traffic/messages?format=json"
           );
           if (!resp.ok) {
             throw new Error(resp.status);
@@ -63,7 +63,7 @@ export default {
       } else {
         try {
           let resp = await fetch(
-            "http://api.sr.se/api/v2/traffic/messages?trafficareaname=" +
+            "https://api.sr.se/api/v2/traffic/messages?trafficareaname=" +
               event.target.value +
               "&format=json&pagination=false"
           );
