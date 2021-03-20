c6408513824afcfda5ab02801bf8b77989734282
adrianbook <adrianbook@gmail.com>
2021-03-20 20:55:37 +0100

    minor fix to unit test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,6 +1,6 @@
 import { expect } from "chai";
-import { shallowMount } from "@vue/test-utils";
-import { iterator } from "core-js/fn/symbol";
+//import { shallowMount } from "@vue/test-utils";
+//import { iterator } from "core-js/fn/symbol";
 //import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("placeholderTest", () => {
