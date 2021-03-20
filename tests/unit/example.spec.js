ef9b247136885495da8becc58129f4e7ef243c15
adrianbook <adrianbook@gmail.com>
2021-03-20 20:31:41 +0100

    added testing test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,7 +1,14 @@
 import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { iterator } from "core-js/fn/symbol";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
+describe("placeholderTest", () => {
+  it("should add upp", () => {
+    expect(1).to.equal(1)
+  });
+});
+/*
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
@@ -11,3 +18,4 @@ describe("HelloWorld.vue", () => {
     expect(wrapper.text()).to.include(msg);
   });
 });
+*/
