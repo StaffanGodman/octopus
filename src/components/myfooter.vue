ba1440e11d90d21a1e6a73dc4a1b93b87dd80c1b
björn munthe <bjomun1011@skola.goteborg.se>
2021-03-22 18:33:22 +0100

    adding more layout and som stuff. Headermenu should be changed

---
+++ src/components/myfooter.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="container">
+    <button class="homebutton" @click="$router.push('/')"></button>
+    <button class="backbutton" @click="$router.back(-1)"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footer",
+}
+</script>
+
+<style scoped>
+#container {
+  background-color: #414141;
+  box-shadow: 1px 1px 4px 0 rgba(0, 0, 0, 0.1);
+  position: fixed;
+  bottom: 0px;
+  width: 100%;
+  height: 3rem;
+}
+
+.homebutton {
+  float: left;
+  background-image: url("../assets/homeicon.png");
+  background-size: cover;
+  height: 2.5rem;
+  width: 2.5rem;
+  margin: 0.3rem;
+}
+.backbutton {
+  float: right;
+  background-image: url("../assets/backarrow.png");
+  background-size: cover;
+  height: 2.5rem;
+  width: 2.5rem;
+  margin: 0.3rem;
+}
+</style>
--- src/views/areaMenu.vue
+++ src/views/areaMenu.vue
@@ -14,81 +14,74 @@
         {{ area.name }}
       </option>
     </select>
-    <div>
-      <p v-for="message in trafficMessages" :key="message">
-        {{ message.title }}
-        {{ message.subcategory }}<br />
-        {{ message.description }}
-      </p>
+    <div class="messagespace" v-if="trafficMessages.length !== 0">
+      <div class="message" v-for="message in trafficMessages" :key="message">
+        <p id="title">{{ message.title }}</p>
+        <p id="category">{{ message.subcategory }}</p>
+        <br />
+        <p id="description">{{ message.description }}</p>
+        <p id="description">{{ message.createddate }}</p>
+      </div>
       <br />
     </div>
   </div>
 </template>
 
 <script>
+import trafficFunctions from "../lib/trafficFunctions.js"
+
 export default {
   data() {
     return {
-      trafficAreas: [],
       trafficMessages: [],
+      trafficAreas: [],
     }
   },
-
   methods: {
-    async getTrafficAreas() {
-      try {
-        let resp = await fetch("https://api.sr.se/api/v2/traffic/areas?format=json&pagination=false")
-        if (!resp.ok) {
-          throw new Error(resp.status)
-        }
-        let json = await resp.json()
-        this.trafficAreas = json.areas
-      } catch (error) {
-        console.error(error)
-      }
-    },
     async getTrafficMessages(event) {
-      if (event.target.value === "all") {
-        try {
-          let resp = await fetch("https://api.sr.se/api/v2/traffic/messages?format=json")
-          if (!resp.ok) {
-            throw new Error(resp.status)
-          }
-          let json = await resp.json()
-          this.trafficMessages = json.messages
-        } catch (error) {
-          console.error(error)
-        }
-      } else {
-        try {
-          let resp = await fetch(
-            "https://api.sr.se/api/v2/traffic/messages?trafficareaname=" +
-              event.target.value +
-              "&format=json&pagination=false"
-          )
-          if (!resp.ok) {
-            throw new Error(resp.status)
-          }
-          let json = await resp.json()
-          this.trafficMessages = json.messages
-        } catch (error) {
-          console.error(error)
-        }
-      }
+      await trafficFunctions.fetchTrafficMessages(event)
+      this.trafficMessages = await trafficFunctions.getTrafficMessages()
     },
   },
-  created() {
-    this.getTrafficAreas()
+  async created() {
+    await trafficFunctions.fetchTrafficAreas()
+    this.trafficAreas = await trafficFunctions.getAreas()
   },
 }
 </script>
 
-<style scoped>
+<style>
 img {
   width: 20rem;
 }
-p {
+.message {
   border: solid lightblue;
   background-color: aliceblue;
+  padding: 0rem;
+  margin: 0rem;
+}
+.messagespace {
+  display: flex;
+  flex-direction: column;
+  border: solid 1px rgb(185, 167, 167);
+  margin: 2rem;
+}
+#title {
+  color: red;
+  font-size: small;
+  float: left;
+  top: 0rem;
+  margin: 0.2rem;
+}
+#category {
+  color: green;
+  font-size: small;
+  float: right;
+  top: 0rem;
+  margin: 0.2rem;
+}
+#desctription {
+  float: left;
+  margin: 0.5rem;
 }
 </style>
