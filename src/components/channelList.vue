276606a4a7b7b4ef3792aac3d2e035204db67eea
Staffan <staffan.godman@gmail.com>
2021-04-06 00:14:19 +0200

    Added functionality for displaying how many times a specific song has been played for each search result

--- src/components/channelList.vue
+++ src/components/channelList.vue
@@ -6,8 +6,9 @@
     </option>
   </select>
   <div>
-    <p id="song" v-for="song in this.songlist" :songlist="songlist" :key="song">
-      {{ song.description }}
+    <p id="song" v-for="song in list" :list="list" :key="song">
+      {{ song.description }}<br />
+      Times played: {{ song.plays }}
     </p>
   </div>
 </template>
@@ -21,11 +22,31 @@ export default {
     return {
       channels: [],
       songlist: [],
+      list: [],
     }
   },
   methods: {
     async channelChosen(event) {
       this.songlist = await songFunctions.fetchSongList(event.target.value)
+      this.createList(this.songlist)
+    },
+    createList(songlist) {
+      console.log(songlist)
+      for (let i = 0; i < songlist.length; i++) {
+        let s = { description: songlist[i].description, plays: 1 }
+        for (let i = 0; i < this.list.length; i++) {
+          if (this.list[i].description === s.description) {
+            console.log("splice")
+            s.plays = this.list[i].plays + 1
+            this.list.splice(i, 1)
+          }
+        }
+        this.list.push(s)
+      }
+      this.list.sort(function(a, b) {
+        return b.plays - a.plays
+      })
+      console.log(this.list)
     },
   },
   async created() {
