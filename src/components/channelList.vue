b418d23433c66795ea61f8168eab5ae5b3966732
Staffan <staffan.godman@gmail.com>
2021-04-06 19:49:36 +0200

    Adding css

--- src/components/channelList.vue
+++ src/components/channelList.vue
@@ -1,15 +1,24 @@
 <template>
-  <select name="Kanal" id="Channels" @change="channelChosen">
-    <option value="placeholder">Välj kanal</option>
-    <option v-for="channel in channels" :key="channel.name" v-bind:value="channel.id">
-      {{ channel.name }}
-    </option>
-  </select>
   <div>
-    <p id="song" v-for="song in list" :list="list" :key="song">
-      {{ song.description }}<br />
-      Times played: {{ song.plays }}
-    </p>
+    <p v-if="this.list.length > 0">Topplistan idag i (placeholder)</p>
+    <br />
+    <div>
+    <select name="Kanal" id="Channels" @change="channelChosen">
+      <option value="placeholder">Välj kanal</option>
+      <option v-for="channel in channels" :key="channel.name" v-bind:value="channel.id">
+        {{ channel.name }}
+      </option>
+    </select>
+    </div>
+    <div class="rounded overflow-scroll m-2 h-50 flex-column" v-if="this.list.length > 0">
+      <p class="bi-text-left colo>Låt</p>
+      <div class="message pb-5" v-for="song in list" :list="list" :key="song">
+        <p id="titleartist">{{ song.description }}</p>
+        <br />
+        <p id="timesplayed">Antal spelningar: {{ song.plays }}</p>
+        <br />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,6 +29,7 @@ export default {
   name: "ChannelList",
   data() {
     return {
+      channelname: "",
       channels: [],
       songlist: [],
       list: [],
@@ -52,3 +62,21 @@ export default {
   },
 }
 </script>
+<style>
+#Channels {
+  position: relative;
+  margin-bottom: 1rem;
+}
+.message {
+  border: solid lightblue;
+  background-color: aliceblue;
+}
+#titleartist {
+  float: left;
+  margin: 0.5rem;
+}
+#timesplayed {
+  float: right;
+  margin: 0.5rem;
+}
+</style>
--- src/views/songMenu.vue
+++ src/views/songMenu.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-    <h1>Låt-Toppen</h1>
+  <div class="color-background vh-100 container-fluid">
+    <h1 class="color-primary-3 fs-1 p-3" >Låt-Toppen</h1>
+    <p class="color-primary-1 text-center">
+      Sök på Låt-Toppen för att se vilken musik som är populärast på just din radiostation!
+    </p>
+    <div>
     <channelList></channelList>
+    </div>
   </div>
 </template>
 
@@ -19,10 +24,5 @@ export default {
 </script>
 
 <style>
-#song {
-  margin: .2em;
-  padding: .2em;
-  border-color: grey;
-  border-style: solid;
-}
+
 </style>
