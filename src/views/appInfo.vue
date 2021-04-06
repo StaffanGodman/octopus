deafff0feeb21697f779a51981b86617f08704d5
björn munthe <bjomun1011@skola.goteborg.se>
2021-04-06 22:20:21 +0200

    some more stuff

--- src/views/appInfo.vue
+++ src/views/appInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-fluid vh-100 color-background text-center p-3">
+  <div class="container-fluid min-size color-background text-center p-3">
     <h1 class=" color-primary-1 ">Vad kan jag göra i den här appen?</h1>
     <div class="container-fluid color3-background rounded p-3">
       I den här appen kan du utforska statistik kopplat till Sveriges Radio. Genom att starta en generator och mata in
